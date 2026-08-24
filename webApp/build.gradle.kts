76ccd64cc847fad181a68b2b483c218feff35532
crispyduck123hc <hcao06@gmail.com>
2026-08-24 16:24:35 +1200

    Config update + added new UI for default sample app (#7)
    * config: add dependencies for date/time feature
    
    enhancing current sample program
    
    * feature: include date time to sample program
    
    new test feature should work on all platforms

--- webApp/build.gradle.kts
+++ webApp/build.gradle.kts
@@ -21,8 +21,8 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(project(":shared"))
-
             implementation(libs.compose.ui)
+            implementation(npm("@js-joda/timezone", "2.25.1"))
         }
     }
 }
--- webApp/src/webMain/kotlin/com/example/notesai/main.kt
+++ webApp/src/webMain/kotlin/com/example/notesai/main.kt
@@ -2,6 +2,9 @@ package com.example.notesai
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+@JsModule("@js-joda/timezone")
+@OptIn(kotlin.js.ExperimentalWasmJsInterop::class)
+external object TimezoneModule
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
